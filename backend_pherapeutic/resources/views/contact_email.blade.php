4091e53d7effb9f77d3d0f950114d6cea509dcc1
Anirudh <anirudh.shukla@xixom.biz>
2021-01-21 19:10:10 +0530

    backend

--- backend_pherapeutic/resources/views/contact_email.blade.php
+++
@@ -1,7 +0,0 @@
-You received an email from : {{ $name }}<br>
-Here are the details:<br>
-<b>Name:</b> {{ $name }}<br>
-<b>Email:</b> {{ $email }}<br>
-<b>Subject:</b> {{ $subject }}<br>
-<b>Message:</b> {{ $msg }}<br>
-Thank You
